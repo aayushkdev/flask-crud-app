53caaeae35e850bdddd31803542f01a50871b787
Aayush Kumar <aayush214.kumar@gmail.com>
2025-06-19 03:17:57 +0530

    fix id not appearing in api response

--- core/db/schemas.py
+++ core/db/schemas.py
@@ -13,6 +13,13 @@ class UserSchema(ma.Schema):
     email = fields.Email(required=True, validate=validate.Length(min=3, max=100))
     password = fields.String(required=True, load_only=True, validate=validate.Length(min=6, max=100)) # we do not return the password in api response
 
+    @post_dump(pass_original=True)
+    # Converts the mongodb `_id` field to `id` before displaying it for a cleaner API output
+    def add_id_field(self, data, original, **kwargs):
+        if "_id" in original:
+            data["id"] = str(original["_id"])
+        return data
+
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 user_partial_schema = UserSchema(partial=True)
